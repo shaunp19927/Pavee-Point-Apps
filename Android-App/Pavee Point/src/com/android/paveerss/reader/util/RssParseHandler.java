b0376ccd045e683f6d97868d50c601b55f9c4417
lopesf <lopesf@tcd.ie>
2014-03-19 04:09:51 +0000

    Format text in news feed

--- Android-App/Pavee Point/src/com/android/paveerss/reader/util/RssParseHandler.java
+++ Android-App/Pavee Point/src/com/android/paveerss/reader/util/RssParseHandler.java
@@ -56,6 +56,7 @@ public class RssParseHandler extends DefaultHandler {
 			parsingLink = false;
 		} else if("description".equals(qName)) {
 			parsingDescription = false;
+			
 		}
 	}
 	
@@ -63,7 +64,7 @@ public class RssParseHandler extends DefaultHandler {
 	public void characters(char[] ch, int start, int length) throws SAXException {
 		if (parsingTitle) {
 			if (currentItem != null)
-				currentItem.setTitle(new String(ch, start, length));
+				currentItem.setTitle(format(new String(ch, start, length)));
 		} else if (parsingLink) {
 			if (currentItem != null) {
 				currentItem.setLink(new String(ch, start, length));
@@ -71,10 +72,31 @@ public class RssParseHandler extends DefaultHandler {
 			}
 		} else if (parsingDescription) {
 			if (currentItem != null) {
-				currentItem.setDescription(new String(ch, start, length));
+				currentItem.setDescription(format(new String(ch, start, length)));
 				parsingDescription = false;
+				
 			}
 		}
+	}
+	
+	private String format(String desc){
+		
+		String new_desc = "";
+		boolean skip = false;
+		
+		for(int i = 0; i < desc.length(); i++) {
+			if(desc.charAt(i) == '<') skip = true;
+			else if(desc.charAt(i) == '>' && skip) skip = false;
+			else if(skip) continue;
+			else if(desc.charAt(i) == '&' && desc.charAt(i+1) == '#') i += 6;
+			else if(desc.charAt(i) == '.') {
+				new_desc += (desc.charAt(i));
+				break;
+			}
+			else new_desc += (desc.charAt(i));
+		}
+		//System.out.println(new_desc);
+		return new_desc;
 	}
 	
 }
