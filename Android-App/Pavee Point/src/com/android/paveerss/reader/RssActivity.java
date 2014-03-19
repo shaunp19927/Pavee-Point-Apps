3513b3b22cffe0c8a884bb06cfc2587a711b3885
lopesf <lopesf@tcd.ie>
2014-03-19 02:41:07 +0000

    News feed is now expandable list
    displays title, child description, on click on child opens browser to webpage

--- Android-App/Pavee Point/src/com/android/paveerss/reader/RssActivity.java
+++ Android-App/Pavee Point/src/com/android/paveerss/reader/RssActivity.java
@@ -1,50 +1,134 @@
 package com.android.paveerss.reader;
 
-
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import com.android.paveerss.reader.data.RssItem;
+import com.android.paveerss.reader.util.RssReader;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
 
-import com.android.paveerss.reader.data.RssItem;
-import com.android.paveerss.reader.listeners.ListListener;
-import com.android.paveerss.reader.util.RssReader;
-
-public class RssActivity extends BaseActivity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.rss_layout);
-		
+ 
+public class RssActivity extends BaseActivity{
+ 
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    List<RssItem> rss;
+    HashMap<String, List<String>> listDataChild;
+    RssReader rssReader;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		setContentView(R.layout.contacts);
+        
 		//Prepare RSS
 		try {
 			// Create RSS reader
-			RssReader rssReader = new RssReader("http://www.paveepoint.ie/feed");
-			// Get a ListView from main view
-			ListView itcItems = (ListView) findViewById(R.id.listMainView);
-
-			// Create a list adapter
-			ArrayAdapter<RssItem> adapter = new ArrayAdapter<RssItem>(this,android.R.layout.simple_list_item_1, rssReader.getItems());
-			
-			// Set list adapter for the ListView
-			itcItems.setAdapter(adapter);
-
-			// Set list view item click listener
-			itcItems.setOnItemClickListener(new ListListener(rssReader.getItems(), this));
+			rssReader = new RssReader("http://www.paveepoint.ie/feed");
+			rss = rssReader.getItems();
+			prepareListData(rss);
 			
 		} catch (Exception e) {
 			Log.e("Pavee RSS Error", e.getMessage());
 		}
-	}
+		
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+ 
+ 
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+ 
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+ 
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+ 
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                    int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+ 
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+ 
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                /*Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();*/
+            }
+        });
+ 
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+ 
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+               /* Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();*/
+ 
+            }
+        });
+        
+     // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                    int groupPosition, int childPosition, long id) {
 
+            	Intent i = new Intent(Intent.ACTION_VIEW);
+            	i.setData(Uri.parse(rss.get(groupPosition).getLink()));
+        		startActivity(i);
+            	
+                return false;
+            }
+            });
+            
+    }
+ 
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData(List<RssItem> list) {
+    	
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+        
+        //add titles of articles
+        for(int i = 0; i < list.size(); i++) {
+        	listDataHeader.add(list.get(i).getTitle());
+        	
+        }
+        
+        //add descriptions
+        for(int i = 0; i < list.size(); i++) {
+        	List<String> temp = new ArrayList<String>();
+        	temp.add(list.get(i).getDescription());
+        	listDataChild.put(listDataHeader.get(i), temp);
+        	
+        }
+       
+    }
+
+    
 }
+
+
