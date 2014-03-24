5e3d39e31ba726be9a7ddfbc9f14009ed09738c9
lopesf <lopesf@tcd.ie>
2014-03-24 13:35:27 +0000

    Added link to paypal page
    But missing actual link - of the form www.paypal.com/aislings_account,
    and not the email address.

--- Android-App/Pavee Point/src/com/android/paveerss/reader/DonateActivity.java
+++ Android-App/Pavee Point/src/com/android/paveerss/reader/DonateActivity.java
@@ -1,9 +1,14 @@
 package com.android.paveerss.reader;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 import android.view.View;
 
 public class DonateActivity extends BaseActivity {
@@ -21,14 +26,30 @@ public class DonateActivity extends BaseActivity {
         btnPaypal.setOnClickListener(new View.OnClickListener() {
  
             public void onClick(View arg0) {
-                //Starting a new Intent
-                Intent nextScreen = new Intent(getApplicationContext(), RssActivity.class);
-                startActivity(nextScreen);
- 
+                
+            	if(isNetworkConnected()) {
+            	//Starting a new Intent
+            	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/webapps/mpp/send-money-online"));
+                startActivity(browserIntent);
+            	} else {
+            		Toast.makeText(getApplicationContext(),
+        		            "Internet Connection Required!",
+        		            Toast.LENGTH_SHORT).show();
+            		
+            	}
             }
         });
 
 	}
 
+    private boolean isNetworkConnected() {
+  	  ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+  	  NetworkInfo ni = cm.getActiveNetworkInfo();
+  	  if (ni == null) {
+  	   // There are no active networks.
+  	   return false;
+  	  } else
+  	   return true;
+  	 }
 
 }
